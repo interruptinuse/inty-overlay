c8ed6e8a798ae98ae25443136ef38fdb70c2f7df
inty <interruptinuse@gmail.com>
2019-04-19 04:16:45 +0000

    install.sh: run unsupervised

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-command -v layman || sudo emerge -av app-portage/layman
-sudo layman -o https://raw.github.com/interruptinuse/inty-overlay/master/repositories.xml -f -a inty-overlay
+command -v layman || sudo emerge -v app-portage/layman
+yes | sudo layman -o https://raw.github.com/interruptinuse/inty-overlay/master/repositories.xml -f -a inty-overlay
