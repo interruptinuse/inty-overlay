385e882f6ae8e377d99fa2436a0912f785c47b43
inty <interruptinuse@gmail.com>
2019-04-19 03:26:03 +0000

    repository.xml -> repositories

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 set -ex
 
 command -v layman || sudo emerge -av app-portage/layman
-sudo layman -o https://raw.github.com/interruptinuse/inty-overlay/master/repository.xml -f -a inty-overlay
+sudo layman -o https://raw.github.com/interruptinuse/inty-overlay/master/repositories.xml -f -a inty-overlay
