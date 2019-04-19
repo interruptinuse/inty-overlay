438475f76fb576cd45a38a9b77829912aa9e1fa7
inty <interruptinuse@gmail.com>
2019-04-19 04:25:10 +0000

    install.sh: run eix-update

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 
 set -ex
 
+REPOXML_URL="https://raw.github.com/interruptinuse/inty-overlay/master/repositories.xml"
+
 command -v layman || sudo emerge -v app-portage/layman
-yes | sudo layman -o https://raw.github.com/interruptinuse/inty-overlay/master/repositories.xml -f -a inty-overlay
+yes | sudo layman -o "${REPOXML_URL}" -f -a inty-overlay
+command -v eix-update && sudo eix-update
