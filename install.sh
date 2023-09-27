c09270057dc7ca1f42eebdcb1b9bba7e697bcc7c
inty <interruptinuse@gmail.com>
2023-09-27 08:26:40 +0000

    Remove install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-REPOXML_URL="https://raw.github.com/interruptinuse/inty-overlay/master/repositories.xml"
-
-command -v layman || sudo emerge -v app-portage/layman
-yes | sudo layman -o "${REPOXML_URL}" -f -a inty-overlay
-command -v eix-update && sudo eix-update
